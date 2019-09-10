608a449cea111934e40448405d1e9e688e7537e5
Magid Drwishah <54711969+MagidDrwishah@users.noreply.github.com>
2019-09-10 20:25:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var div1 =document.createElement("div");
+document.body.appendChild(div1);
+var para = document.createElement("P");            
+para.innerHTML = "Magid Drwishah";                       
+div1.appendChild(para);                     
+var div2 = document.createElement("div");
+document.body.appendChild(div2);
+var arr = ["js"," html"," reading"," boxning"];
+div2.innerHTML= arr;
